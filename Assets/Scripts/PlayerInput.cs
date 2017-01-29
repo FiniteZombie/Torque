34b83d161c75ee45427166f47347c6aa9c7cd2d8
FiniteZombie <edi.ramirez@gmail.com>
2017-01-29 02:10:25 -0800

    Basic driving
    Added a car and controls for moving it side to side.

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+    
+    public float RoadSize;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0) {
+            var touch = Input.GetTouch(0);
+            Touch(touch.position);
+        }
+
+        if (Input.GetMouseButton(0)) {
+            Touch(Input.mousePosition);
+        }
+	}
+
+    public void Touch(Vector2 touchPosition) {
+        var position = transform.position;
+        var viewPos = touchPosition.x / Screen.width;
+        var roadPos = 2 * (viewPos - .5f);
+        position.x = RoadSize * roadPos;
+        transform.position = position;
+    }
+}

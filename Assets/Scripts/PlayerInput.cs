4c3136c1af87bd5e240f422688f453e0818ae89f
FiniteZombie <edi.ramirez@gmail.com>
2017-02-12 19:27:48 -0800

    Car now rotates and turns via virtual steering wheel

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,6 +3,12 @@
 public class PlayerInput : MonoBehaviour {
     
     public float RoadSize;
+    public float WheelPosition = .75f;
+    public float WheelSize = .4f;
+    public float Handling = 1f;
+    public float MaxTurnAngle = 45f;
+
+    private float hSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -11,21 +17,40 @@ public class PlayerInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        var wheelPos = 0f;
+
         if (Input.touchCount > 0) {
             var touch = Input.GetTouch(0);
-            Touch(touch.position);
+            wheelPos = GetWheelPosition(touch.position);
         }
 
         if (Input.GetMouseButton(0)) {
-            Touch(Input.mousePosition);
+            wheelPos = GetWheelPosition(Input.mousePosition);
         }
+
+        Steer(wheelPos);
 	}
 
-    public void Touch(Vector2 touchPosition) {
+    void FixedUpdate() {
         var position = transform.position;
-        var viewPos = touchPosition.x / Screen.width;
-        var roadPos = 2 * (viewPos - .5f);
-        position.x = RoadSize * roadPos;
+        position.x += hSpeed;
+        position.x = Mathf.Clamp(position.x, -.5f * RoadSize, .5f * RoadSize);
+
         transform.position = position;
+    }
+
+    public float GetWheelPosition(Vector2 touchPosition) {
+        var viewPos = touchPosition.x / Screen.width;
+        var val = viewPos - WheelPosition;
+        return val / WheelSize;
+    }
+
+    public void Steer(float amount) {
+        hSpeed = Handling * amount;
+
+        var turnAngle = MaxTurnAngle * amount;
+        var carAngle = -90f + turnAngle;
+        var rotation = Quaternion.Euler(0, carAngle, 0);
+        transform.rotation = rotation;
     }
 }

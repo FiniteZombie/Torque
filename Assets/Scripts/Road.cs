b46a401422c324a214669ed321e965f85c0d710e
FiniteZombie <edi.ramirez@gmail.com>
2017-03-06 01:05:32 -0800

    Added texture offset control for road

---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour {
+
+    [Range (-1f, 0f)]
+    public float Offset;
+
+    private Renderer _renderer;
+
+    // Use this for initialization
+    void Start () {
+        _renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _renderer.material.SetTextureOffset("_MainTex", new Vector2(0, Offset));
+    }
+}
